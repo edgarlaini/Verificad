c325d3c8b3e680bfff6322b2f1d680f5d44ecb87
edgarlaini <edgar.laini@gmail.com>
2026-08-20 14:25:30 +0200

    Add files via upload

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -8,7 +8,7 @@ let client: Stripe | null = null;
 export function getStripe(): Stripe {
   if (!client) {
     client = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: "2024-06-20",
+      apiVersion: "2025-02-24.acacia",
     });
   }
   return client;
